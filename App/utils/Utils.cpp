375c7fb2b1c060d5e4122239e140cdad53a8cdc7
Abel <abel.perez@gmail.com>
2026-06-08 22:39:03 +0200

    Change nanosleep

--- App/utils/Utils.cpp
+++ App/utils/Utils.cpp
@@ -1,6 +1,13 @@
-
 #include "Utils.h"
 
+
+#include <unistd.h>
+
+
+/**
+ * Sleep for the specified number of milliseconds, dont use std::this_thread::sleep_for(). Is broken.
+ * @param milliseconds
+ */
 void Utils::sleepFor(int milliseconds) {
-  std::this_thread::sleep_for(std::chrono::milliseconds(milliseconds));
+  usleep(milliseconds  * 1000);
 }
